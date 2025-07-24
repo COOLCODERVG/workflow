6ea5d086d25fc4324df91aa2778bd60584112630
Varshith Gude <varshith.gudeus@gmail.com>
2025-07-24 00:39:41 -0700

    umm. emphasis

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -8,8 +8,8 @@ import picture from "./assets/bg.png";
 import logo from "./assets/logo.png";
 
 const Loginform = () => {
-  const [email, setEmail] = useState("test@gmail.com");
-  const [password, setPassword] = useState("test");
+  const [email, setEmail] = useState("demouser@workflow.com");
+  const [password, setPassword] = useState("Testuser!");
   const [err, setErr] = useState("");
 
   const history = useNavigate();
--- frontend/src/forgotPassword.tsx
+++ frontend/src/forgotPassword.tsx
@@ -58,7 +58,7 @@ const ForgotPasssword = () => {
           </Button>
         </form>
         <h2 className="absolute bottom-[1vw] left-[1vw] text-gray-200 text-[1.3ch]">
-          Copyright © Dyne Research 2024. All rights reserved.
+          Copyright © Varshith Gude 2025. All rights reserved.
         </h2>
       </div>
       <img className="w-[60%] object-cover object-right" src={picture} alt="Background" />
